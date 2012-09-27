62d35ad75d60315a674db5203cb03484971e5ab6
unknown <supersat@cs.washington.edu>
2012-09-26 17:04:08 -0700

    Add firmware

---
+++ firmware/firmware.c
@@ -0,0 +1,17 @@
+/*
+ * firmware.c
+ *
+ * Created: 9/22/2012 8:56:58 PM
+ *  Author: supersat
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
